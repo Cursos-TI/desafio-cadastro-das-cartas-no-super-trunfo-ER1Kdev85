89eee5ff475d1627491b0c1044b93835a2c727ff
ER1Kdev85 <ericleslima2002@gmail.com>
2025-04-08 23:34:00 +0000

    att

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
+char estado;
+char id[4];
+char nome[50];
+int populaçao;
+int pontost;
+float area;
+float pib;
 
 
 int main() {
     printf("Seja bem vindo(a) ao Cartas Trunfo! \n");
 
-   
+    printf("Vamos começar com a CARTA 1. Digite o estado: \n");
+    scanf("%s", estado);  
 
     return 0;
 }
