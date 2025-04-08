a8fe0e268f09a67c2ca00cb3c02417be009958f9
ER1Kdev85 <ericleslima2002@gmail.com>
2025-04-08 23:45:56 +0000

    att

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 char estado;
 char id[4];
 char nome[50];
@@ -13,7 +14,28 @@ int main() {
     printf("Seja bem vindo(a) ao Cartas Trunfo! \n");
 
     printf("Vamos começar com a CARTA 1. Digite o estado: \n");
-    scanf("%s", estado);  
+    scanf("%s", &estado);  
+
+    printf("Ok. Agora me diga o ID da carta: \n");
+    scanf("s%", &id);
+
+    printf("òtimo, pode me informar o nome d cidade? \n");
+    scanf("s%", &nome);
+
+    printf("Qual a população atual da cidade? \n");
+    scanf("d%", &populaçao);
+
+    printf("Perfeito! Consegue me informar a quantidade de pontos turísticos da cidade? \n");
+    scanf("d%", &pontost);
+
+    printf("Qual a área total da cidade em km2? \n");
+    scanf("f%", &area);
+
+    printf("Por último, qual o PIB da cidade? \n");
+    scanf("f%", &pib);
+    
+
+
 
     return 0;
 }
